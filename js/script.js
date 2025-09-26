56d3615c0dbde2643dbfacd3e239dfe0f0c6beb0
Edwin <edwin.rochaquezada92@gmail.com>
2025-09-25 19:53:34 -0600

    change event

--- js/script.js
+++ js/script.js
@@ -177,7 +177,7 @@ subfamilySelect.addEventListener('change', () => {
   const music = document.getElementById("bg-music");
 
   // cuando el usuario haga scroll por primera vez
-  window.addEventListener("mousemove", () => {
+  window.addEventListener("click", () => {
     music.currentTime = 7;   // empieza en el segundo 7
     music.muted = false;      // aseguramos que suene
     music.volume = 0.5;       // volumen medio
