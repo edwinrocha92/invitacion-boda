58496eb44f8f0ba8e904bcebcb692c8fa1f486a2
Edwin <edwin.rochaquezada92@gmail.com>
2025-10-02 11:09:13 -0600

    add familiy

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ setInterval(updateCountdown, 1000);
 
     const families = {
       Issela: { Familia_Rocha_Quezada: 5, Familia_Gonzalez_Quezada: 2, Familia_Hernandez_Quezada: 3, Familia_Rosales_Quezada: 6, Familia_Rios_Quezada: 4, Familia_Rocha_Nader: 2, Familia_Guerrero_Lopez: 4, Familia_Molina_Franco:2, Familia_Hernandez_Melendrez:2, Familia_Crissanto_Hernandez:2, Familia_Gonzalez_Gonzalez:2, Familia_Appleberry:2},
-      Carlos: { Familia_Avitia_Hernandez: 5, Familia_Perales_Avitia: 3,Familia_Rodriguez_Hernandez:2,Familia_Herrera_Hernandez:2, Familia_Hernandez_Cabrales:3, Familia_Esquivel_Hernandez:2, Familia_Hernandez_De_La_Hoya:2, Familia_Hernandez_Silerio:2,Familia_Hernandez_Arambula:5, Fam_Mata_Hernandez:2, Fam_Espinoza_Hernandez:6, Familia_Felix:3}
+      Carlos: { Familia_Avitia_Hernandez: 5, Familia_Perales_Avitia: 3,Familia_Rodriguez_Hernandez:2,Familia_Herrera_Hernandez:2, Familia_Hernandez_Cabrales:3, Familia_Esquivel_Hernandez:2, Familia_Hernandez_De_La_Hoya:2, Familia_Hernandez_Silerio:2,Familia_Hernandez_Arambula:5, Fam_Mata_Hernandez:2, Fam_Espinoza_Hernandez:6, Familia_Felix:3, Familia_Torres_Hernandez:3}
     };
 
     // Nombres legibles para mostrar en dropdown y WhatsApp
@@ -65,7 +65,8 @@ setInterval(updateCountdown, 1000);
       Familia_Hernandez_Arambula: "Familia Hernández Arámbula",
       Fam_Mata_Hernandez: "Familia Mata Hernández",
       Fam_Espinoza_Hernandez: "Familia Espinoza Hernández",
-      Familia_Felix: "Familia Félix"
+      Familia_Felix: "Familia Félix",
+      Familia_Torres_Hernandez: "Familia Torres Hernández"
 
     };
 
